125244f8377a4adbfb293d21d59316e060017a61
Antony Figueroa <antonysamuel0903@gmail.com>
2026-01-20 17:31:08 -0400

    ajuste de el dashboard home y las stacts que se muestran
    tambien se añadieron estadisticas

--- backend/src/routes/tracking.routes.ts
+++ backend/src/routes/tracking.routes.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
-import { getTrackings, createTracking, updateTracking, deleteTracking } from '../controllers/tracking.controller.js';
+import { 
+  getTrackings, 
+  createTracking, 
+  updateTracking, 
+  deleteTracking,
+  getTrackingStats
+} from '../controllers/tracking.controller.js';
 
 const router = Router();
 
 router.get('/', getTrackings);
+router.get('/stats', getTrackingStats);
 router.post('/', createTracking);
 router.put('/:id', updateTracking);
 router.delete('/:id', deleteTracking);

ddbcbbad96d44d36fd76338bd8cdc0af7bc03daf
Antony Figueroa <114882234+Antony-Figueroa@users.noreply.github.com>
2026-07-26 17:12:22 -0400

    chore: clean repo for deployment

--- backend/check-pass.cjs
+++
@@ -1,3 +0,0 @@
-const bcrypt = require('bcryptjs');
-const hash = '$2b$10$FwPfuoFovP9hqFJiaVBHMOGedhHlIo6ZvCQVYgm8pybR0G4CLLhB6';
-bcrypt.compare('Admin123!', hash).then(m => console.log('Password match:', m));
